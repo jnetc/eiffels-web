c569946e89a9b9c563c0b07455332bfb9512a9e5
Anton <frontendevs@gmail.com>
2024-06-17 18:36:36 +0300

    added more sprite icons, created popever switch case by file folders and some basic style for it

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   // Toggle left side menu
   const menuButton = document.querySelector('.btn-menu')
   const dashboard = document.querySelector('.dashboard__wrapper')
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     dashboard.classList.toggle('hidden')
   })
 
+  // Change active state of filter buttons
   const filterButtons = document.querySelectorAll('.fm-filters__btn')
 
   filterButtons.forEach((button) => {
@@ -17,6 +19,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
       filterButtons.forEach((btn) => btn.classList.remove('btn-selected'))
       button.classList.add('btn-selected')
+    })
+  })
+
+  // Modal windows for different file types
+  const popoverButons = document.querySelectorAll('[popovertarget]')
+  const imageFile = document.querySelector('#image-file')
+  const pdfFile = document.querySelector('#pdf-file')
+  const videoFile = document.querySelector('#video-file')
+  const audioFile = document.querySelector('#audio-file')
+
+  popoverButons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // Get filename from data-file attribute
+      const filename = button.closest('.fm-tb__fm-row').querySelector('.fm-row__name').dataset.file
+      // Get folder name from data-file attribute
+      // Folder name is the same as the "public" folder
+      const folder = button.closest('.fm-tb__fm-row').querySelector('.fm-row__name').dataset.source
+      // Set source
+      switch (folder) {
+        case 'image':
+          imageFile.querySelector('img').setAttribute('src', `./public/${folder}/${filename}`)
+          break;
+        case 'pdf':
+          pdfFile.querySelector('iframe').setAttribute('src', `./public/${folder}/${filename}`)
+          break;
+        case 'video':
+          videoFile.querySelector('video').setAttribute('src', `./public/${folder}/${filename}`)
+          break;
+        case 'audio':
+          audioFile.querySelector('audio').setAttribute('src', `./public/${folder}/${filename}`)
+          break;
+        default:
+          break;
+      }
     })
   })
 })
