53b990e9ee11690b90c119d88efb5741cb816726
Anton <frontendevs@gmail.com>
2024-07-29 22:04:40 +0300

    auth page mostly completed

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,34 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const authForm = document.querySelector('.auth__form');
 	const digits = document.querySelectorAll('[inputmode="numeric"]');
-	// const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	const submitButton = document.querySelector('.auth__btn');
 
 	digits.forEach((input, index) => {
 		input.addEventListener('input', () => {
-			// if (!/^\d$/.test(input.value)) {
-			// 	input.value = '';
-			// 	input.focus();
-			// 	console.log('Only digits are allowed');
-			// }
-			if (!input.value) {
-				input.value = '';
-				input.focus();
-				console.log('start off');
-			}
+			// Only keep the last entered digit
+			input.value = input.value.replace(/[^0-9]/g, '').slice(-1);
 
-			if (input.value.length > 1) {
-				input.value = input.value.split('')[1];
-			}
-
-			if (index < digits.length - 1) {
-				console.log('end off');
+			// If a digit is entered, move the focus to the next input, if it exists
+			if (input.value.length === 1 && index < digits.length - 1) {
 				digits[index + 1].focus();
 			}
+		});
 
-			if (index === digits.length - 1) {
-				console.log('end off');
-				digits[index].blur();
+		// Prevent invalid characters from being pasted
+		input.addEventListener('paste', event => {
+			const paste = (event.clipboardData || window.clipboardData).getData('text');
+			event.preventDefault();
+
+			// Ensure pasted content is exactly 6 digits
+			if (!/^[0-9]{6}$/.test(paste)) return;
+
+			// Fill each input with the corresponding digit from the pasted value
+			digits.forEach((input, index) => {
+				input.value = paste[index];
+			});
+
+			// Move focus to the submit button after handling the paste event
+			const breakPasteActivity = setTimeout(() => {
+				submitButton.focus();
+				clearTimeout(breakPasteActivity);
+			}, 0);
+		});
+
+		// We prevent the input of inappropriate characters through the keyboard
+		input.addEventListener('keypress', event => {
+			if (!/^[0-9]$/.test(event.key)) {
+				event.preventDefault();
 			}
 		});
+
+		// We prevent the carriage from moving to the end of the field when clicked
+		input.addEventListener('focus', event => {
+			setTimeout(() => {
+				input.setSelectionRange(0, 1);
+			}, 0);
+		});
+	});
+
+	authForm.addEventListener('submit', event => {
+		event.preventDefault();
+		console.log(
+			'phone number',
+			digits[0].value + digits[1].value + digits[2].value + digits[3].value + digits[4].value + digits[5].value
+		);
+		window.location.href = 'logged.html';
 	});
 });
