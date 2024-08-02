4d037758f0fe44d56971dfb3b4c51476b378771f
Anton <frontendevs@gmail.com>
2024-08-02 11:57:17 +0300

    account ui markup is completed

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('Settings loaded');
+	const list = document.querySelector('.settings__list');
+	const listItems = document.querySelectorAll('.settings__name');
+
+	console.log(listItems);
+
+	function collapseList(event) {
+		const item = event.target;
+
+		if (item.classList.contains('open-settings')) {
+			item.classList.remove('open-settings');
+			return;
+		}
+
+		// Collapse all summaries without target
+		listItems.forEach(el => {
+			if (el.classList.contains('open-settings')) {
+				el.classList.remove('open-settings');
+				return;
+			}
+		});
+
+		item.classList.add('open-settings');
+	}
+
+	listItems.forEach(item => {
+		item.addEventListener('pointerup', collapseList);
+	});
 });
