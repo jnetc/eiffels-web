057119c70446a7108205e961e2ef8ce8323a4424
Anton <frontendevs@gmail.com>
2024-08-10 17:15:52 +0300

    finded and updated css issues, added pricing, collapse list fix in settings

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const heroAuthForm = document.querySelector('.unlogged__form');
-	const phoneNumberInput = document.getElementById('unlogged__phone-number');
-	const errorMessage = document.querySelector('.error-message');
-	let phoneNumber = '';
+document.addEventListener("DOMContentLoaded", () => {
+	const heroAuthForm = document.querySelector(".unlogged__form");
+	const phoneNumberInput = document.getElementById("unlogged__phone-number");
+	const errorMessage = document.querySelector(".error-message");
+	let phoneNumber = "";
 
-	// Immediately focus on phone number input
-	phoneNumberInput.focus();
+	if (heroAuthForm) {
+		heroAuthForm.addEventListener("submit", (event) => {
+			event.preventDefault();
 
-	heroAuthForm.addEventListener('submit', event => {
-		event.preventDefault();
+			console.log("phone number", phoneNumber);
+			window.location.href = "auth.html";
+		});
+	}
 
-		console.log('phone number', phoneNumber);
-		window.location.href = 'auth.html';
-	});
+	if (phoneNumberInput) {
+		// Immediately focus on phone number input
+		phoneNumberInput.focus();
 
-	phoneNumberInput.addEventListener('input', event => {
-		// If input is empty
-		if (event.target.value === '') {
-			errorMessage.classList.remove('show');
-			return;
-		}
+		phoneNumberInput.addEventListener("input", (event) => {
+			// If input is empty
+			if (event.target.value === "") {
+				errorMessage.classList.remove("show");
+				return;
+			}
 
-		// If intered value is not number
-		if (!/^[+][0-9]{0,}$/.test(event.target.value)) {
-			errorMessage.classList.add('show');
-			return;
-		}
+			// If intered value is not number
+			if (!/^[+][0-9]{0,}$/.test(event.target.value)) {
+				errorMessage.classList.add("show");
+				return;
+			}
 
-		errorMessage.classList.remove('show');
-		phoneNumber = event.target.value;
-	});
+			errorMessage.classList.remove("show");
+			phoneNumber = event.target.value;
+		});
+	}
 });
--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
 	// Sign Out element
-	const signOut = document.getElementById('settings__sign-out');
+	const signOut = document.getElementById("settings__sign-out");
 
 	// Add user picture
-	const openDialogAUP = document.getElementById('open__aup-dialog');
-	const dialogAUP = document.getElementById('aup-dialog');
-	const formAUP = document.getElementById('aup__form');
+	const openDialogAUP = document.getElementById("open__aup-dialog");
+	const dialogAUP = document.getElementById("aup-dialog");
+	const formAUP = document.getElementById("aup__form");
 
 	// Select all elements with the class 'settings__name'
-	const listItems = document.querySelectorAll('.settings__name');
+	const listItems = document.querySelectorAll(".settings__name");
 
 	// ------------------------------
 	// SETTINGS LIST ITEMS
@@ -17,65 +17,67 @@ document.addEventListener('DOMContentLoaded', () => {
 		const item = event.target;
 
 		// Collapse item if it is open
-		if (item.classList.contains('open-settings')) {
-			item.classList.remove('open-settings');
+		if (item.classList.contains("open-settings")) {
+			item.classList.remove("open-settings");
 			return;
 		}
 
 		// Collapse all summaries without target
 		for (const el of listItems) {
-			if (el.classList.contains('open-settings')) {
-				el.classList.remove('open-settings');
-				return;
+			if (el.classList.contains("open-settings")) {
+				el.classList.remove("open-settings");
 			}
 		}
 
 		// Open the clicked item
-		item.classList.add('open-settings');
+		item.classList.add("open-settings");
 	}
 
 	// Add 'click' event listener to each connection element
 	for (const item of listItems) {
-		item.addEventListener('click', collapseList);
+		item.addEventListener("click", collapseList);
 	}
 
 	// Sign Out user on submit and redirect to index
-	signOut.addEventListener('submit', event => {
+	signOut.addEventListener("submit", (event) => {
 		event.preventDefault();
-		window.location.href = 'index.html';
+		window.location.href = "index.html";
 	});
 
 	// ------------------------------
 	// OPEN DIALOG "ADD USER PICTURE"
-	openDialogAUP.addEventListener('click', () => {
+	openDialogAUP.addEventListener("click", () => {
 		// Get image from UI and display it in the dialog
-		document.getElementById('aup__photo-display').src = document.getElementById('profile__picture').src;
+		document.getElementById("aup__photo-display").src =
+			document.getElementById("profile__picture").src;
 
-		document.body.style.overflow = 'hidden';
+		document.body.style.overflow = "hidden";
 		dialogAUP.showModal();
 	});
 
 	// Close dialog if cancel button or close button is clicked
 	function cancelOrCloseDialogAUP() {
-		const file = dialogAUP.querySelector('#aup__picture-file');
-		const preview = dialogAUP.querySelector('#aup__photo-display');
-		const errorMessage = dialogAUP.querySelector('.error-message');
+		const file = dialogAUP.querySelector("#aup__picture-file");
+		const preview = dialogAUP.querySelector("#aup__photo-display");
+		const errorMessage = dialogAUP.querySelector(".error-message");
 
 		// Clear selected file
-		file.value = '';
-		preview.src = '';
+		file.value = "";
+		preview.src = "";
 
 		// Hide error message
-		errorMessage.classList.remove('show');
+		errorMessage.classList.remove("show");
 
-		document.body.removeAttribute('style');
+		document.body.removeAttribute("style");
 		dialogAUP.close();
 	}
 
-	dialogAUP.querySelector('#aup-dialog__close-btn').addEventListener('click', cancelOrCloseDialogAUP);
+	dialogAUP
+		.querySelector("#aup-dialog__close-btn")
+		.addEventListener("click", cancelOrCloseDialogAUP);
 
 	async function getImageData() {
-		document.addEventListener('change', event => {
+		document.addEventListener("change", (event) => {
 			const file = event.target.files[0];
 
 			if (file) {
@@ -83,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				reader.onload = (e) => {
 					// Display the image in the UI by setting the source of the image element
-					document.getElementById('aup__photo-display').src = e.target.result;
+					document.getElementById("aup__photo-display").src = e.target.result;
 				};
 				// Read the selected file as a Data URL
 				reader.readAsDataURL(file);
@@ -92,25 +94,28 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// Select file and preview image
-	formAUP.querySelector('#aup__picture-file').addEventListener('click', getImageData);
+	formAUP
+		.querySelector("#aup__picture-file")
+		.addEventListener("click", getImageData);
 
-	formAUP.addEventListener('submit', event => {
+	formAUP.addEventListener("submit", (event) => {
 		event.preventDefault();
-		const button = event.target.querySelector('button');
-		const file = event.target.querySelector('#aup__picture-file');
+		const button = event.target.querySelector("button");
+		const file = event.target.querySelector("#aup__picture-file");
 		// When the form is submitted, disable the button and show a loading indicator
 		file.disabled = true;
 		button.disabled = true;
-		button.querySelector('.btn-text').textContent = 'Uploading Image';
+		button.querySelector(".btn-text").textContent = "Uploading Image";
 
 		// Emulate save on server
 		const timeout = setTimeout(() => {
 			// Show selected image in UI after submitting form
-			document.getElementById('profile__picture').src = document.getElementById('aup__photo-display').src;
+			document.getElementById("profile__picture").src =
+				document.getElementById("aup__photo-display").src;
 			// Close dialog and reset form
 			cancelOrCloseDialogAUP();
 
-			button.firstElementChild.textContent = 'Upload Image';
+			button.firstElementChild.textContent = "Upload Image";
 			file.disabled = false;
 			button.disabled = false;
 
