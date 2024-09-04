c07c106f324e8637999fac26204eda33d6c59ea7
Anton <frontendevs@gmail.com>
2024-09-04 16:38:34 +0300

    update nav menu and header

--- scripts/index/navigation.js
+++ scripts/index/navigation.js
@@ -1,15 +1,21 @@
 export default function navigation() {
     // Кнопка мобильного меню и контейнер меню
-    const mobMenuButton = document.getElementById('header__mob-menu-btn');
+    const openMenuButton = document.getElementById('header__open-mob-menu');
+    const closeMenuButton = document.getElementById('header__close-mob-menu');
     const mobNavigation = document.querySelector('.header__nav');
     // ------------------------------
     // МОБИЛЬНОЕ МЕНЮ
     // Обработка клика по кнопке мобильного меню
-    mobMenuButton.addEventListener('click', () => {
-        mobNavigation.classList.toggle('open');
+    openMenuButton.addEventListener('click', () => {
+        mobNavigation.classList.add('open');
         mobNavigation.classList.contains('open')
             ? addEventsToLinks(true) // Если меню открыто, добавляем события клика
             : addEventsToLinks(false); // Если меню закрыто, убираем события клика
+    });
+    // Обработка клика по кнопке мобильного меню
+    closeMenuButton.addEventListener('click', () => {
+        console.log('close');
+        mobNavigation.classList.remove('open');
     });
     // Функция для добавления или удаления событий клика на ссылки
     function addEventsToLinks(boolean) {
--- src/index/navigation.ts
+++ src/index/navigation.ts
@@ -1,17 +1,24 @@
 export default function navigation() {
   // Кнопка мобильного меню и контейнер меню
-  const mobMenuButton = document.getElementById('header__mob-menu-btn') as HTMLButtonElement;
+  const openMenuButton = document.getElementById('header__open-mob-menu') as HTMLButtonElement;
+  const closeMenuButton = document.getElementById('header__close-mob-menu') as HTMLButtonElement;
   const mobNavigation = document.querySelector('.header__nav') as HTMLElement;
 
   // ------------------------------
   // МОБИЛЬНОЕ МЕНЮ
 
   // Обработка клика по кнопке мобильного меню
-  mobMenuButton.addEventListener('click', () => {
-    mobNavigation.classList.toggle('open');
+  openMenuButton.addEventListener('click', () => {
+    mobNavigation.classList.add('open');
     mobNavigation.classList.contains('open')
       ? addEventsToLinks(true) // Если меню открыто, добавляем события клика
       : addEventsToLinks(false); // Если меню закрыто, убираем события клика
+  });
+  // Обработка клика по кнопке мобильного меню
+  closeMenuButton.addEventListener('click', () => {
+    console.log('close');
+
+    mobNavigation.classList.remove('open');
   });
 
   // Функция для добавления или удаления событий клика на ссылки
